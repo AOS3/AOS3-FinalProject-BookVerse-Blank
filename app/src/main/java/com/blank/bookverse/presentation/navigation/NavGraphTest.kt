6da22d20ea34e5c0d23c8a8098a6d4fc45275205
StopStone <wltjr6432@naver.com>
2025-02-12 14:55:12 +0900

    .

--- app/src/main/java/com/blank/bookverse/presentation/navigation/NavGraphTest.kt
+++ app/src/main/java/com/blank/bookverse/presentation/navigation/NavGraphTest.kt
@@ -3,26 +3,22 @@ package com.blank.bookverse.presentation.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.BlendMode.Companion.Screen
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.blank.bookverse.presentation.ui.book_detail.BookDetailScreen
 import com.blank.bookverse.presentation.ui.findAccount.FindAccountScreen
+import com.blank.bookverse.presentation.ui.home.HomeScreen
 import com.blank.bookverse.presentation.ui.login.LoginScreen
+import com.blank.bookverse.presentation.ui.more_qoute.MoreQuoteScreen
 import com.blank.bookverse.presentation.ui.register.RegisterScreen
 import com.blank.bookverse.presentation.ui.splash.SplashScreen
-import com.blank.bookverse.presentation.ui.login.LoginScreen
-import com.blank.bookverse.presentation.ui.splash.SplashScreen
-import com.blank.bookverse.presentation.ui.book_detail.BookDetailScreen
-import com.blank.bookverse.presentation.ui.home.HomeScreen
-import com.blank.bookverse.presentation.ui.more_qoute.MoreQuoteScreen
 
 @Composable
 fun NavGraphTest(navController: NavHostController, modifier: Modifier = Modifier) {
     NavHost(
         navController = navController,
-        startDestination = BottomNavItem.Splash.route,
+        startDestination = MainNavItem.Splash.route,
         modifier = modifier
     ) {
 //        // 홈 화면
