bf186dd3aa0418acb344cac56d947fae5fb300cb
jaeminKimPea <kg8480@gmail.com>
2025-02-17 11:08:52 +0900

    rebase

--- app/src/main/java/com/blank/bookverse/presentation/navigation/NavGraphTest.kt
+++ app/src/main/java/com/blank/bookverse/presentation/navigation/NavGraphTest.kt
@@ -20,7 +20,7 @@ import com.blank.bookverse.presentation.ui.splash.SplashScreen
 fun NavGraphTest(navController: NavHostController, modifier: Modifier = Modifier) {
     NavHost(
         navController = navController,
-        startDestination = MainNavItem.QuoteWrite.route,
+        startDestination = MainNavItem.Splash.route,
         modifier = modifier
     ) {
 //        // 홈 화면
