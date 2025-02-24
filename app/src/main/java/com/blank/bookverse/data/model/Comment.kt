51a2a657fb3db781c3ad8323260c1525d6742b40
StopStone <wltjr6432@naver.com>
2025-02-24 10:48:07 +0900

    PropertyName 지정

--- app/src/main/java/com/blank/bookverse/data/model/Comment.kt
+++ app/src/main/java/com/blank/bookverse/data/model/Comment.kt
@@ -1,8 +1,11 @@
 package com.blank.bookverse.data.model
 
+import com.google.firebase.firestore.PropertyName
+
 data class Comment(
-    val commentDocId: String = "",
-    val quoteDocId: String = "",
-    val commentContent: String = "",
-    val createdAt: Long = System.currentTimeMillis()
+    @PropertyName("comment_doc_id") val commentDocId: String = "",
+    @PropertyName("quote_doc_id") val quoteDocId: String = "",
+    @PropertyName("comment_content") val commentContent: String = "",
+    @PropertyName("is_delete") val isDelete: Boolean = false,
+    @PropertyName("created_at") val createdAt: Long = System.currentTimeMillis(),
 )
