3a44b7892316e916d8d730e87490c4c7ca75ef61
StopStone <wltjr6432@naver.com>
2025-03-04 16:48:12 +0900

    글귀 상세에서 이미지 삭제 롤백

--- app/src/main/java/com/blank/bookverse/presentation/ui/quote_detail/QuoteDetailScreen.kt
+++ app/src/main/java/com/blank/bookverse/presentation/ui/quote_detail/QuoteDetailScreen.kt
@@ -139,6 +139,18 @@ fun QuoteDetailContent(
                             .border(0.5.dp, Color.LightGray)
                     )
 
+                    CoilImage(
+                        modifier = Modifier
+                            .padding(horizontal = 42.dp)
+                            .fillMaxWidth()
+                            .height(220.dp)
+                            .background(Color.Gray),
+                        imageModel = { uiState.quoteDetail?.photoUrl },
+                        imageOptions = ImageOptions(
+                            contentScale = ContentScale.Crop,
+                        )
+                    )
+
                     Spacer(modifier = Modifier.height(18.dp))
                     Text(
                         text = uiState.quoteDetail?.quoteContent ?: "",
