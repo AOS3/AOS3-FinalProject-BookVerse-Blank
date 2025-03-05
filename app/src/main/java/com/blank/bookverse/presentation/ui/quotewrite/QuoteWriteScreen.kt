f6c9211826939ed04b6c99f9f94651f2879f1b66
jaeminKimPea <kg8480@gmail.com>
2025-03-05 10:29:28 +0900

    rebase

--- app/src/main/java/com/blank/bookverse/presentation/ui/quotewrite/QuoteWriteScreen.kt
+++ app/src/main/java/com/blank/bookverse/presentation/ui/quotewrite/QuoteWriteScreen.kt
@@ -208,7 +208,7 @@ fun QuoteWriteScreen(
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .weight(4f)
+                        .height(200.dp)
                         .background(Color.LightGray),
                 ) {
                     AsyncImage(
@@ -219,7 +219,7 @@ fun QuoteWriteScreen(
                     )
                 }
                 Row(
-                    modifier = Modifier.weight(1f),
+                    modifier = Modifier,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     LazyRow(
@@ -258,9 +258,11 @@ fun QuoteWriteScreen(
 
                 context.openFileInput(captureName)
                 HorizontalDivider()
+                val textDp = with(density){screenHeight.toDp()}-367.dp
+                Log.d("st","$textDp")
                 QuoteWriteTextField(
                     text = viewModel.quoteText,
-                    modifier = Modifier.weight(10f),
+                    modifier = Modifier.height(textDp),
                     placeholder = "마음에 드는 글귀를 적어주세요. (필수)",
                     input = viewModel.writeEnabled,
                     screenHeight = screenHeight,
@@ -297,8 +299,7 @@ fun QuoteWriteScreen(
 //                }
                 BookVerseButton(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f),
+                        .fillMaxWidth(),
                     text = textComplete,
                     onClick = {
                         // 작성 완료
